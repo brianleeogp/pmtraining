458177f28d996c8433ebf2f8252bb3af34b90cb1
Brian Lee <>
2023-10-06 15:59:17 +0800

    removing space bar

--- server.js
+++ server.js
@@ -26,9 +26,10 @@ app.get("/", function (req, res) {
 app.get("/work", function (req, res) {
   const projects = [
     { name: "ExploreSG", goal: "An interactive web-app for social studies/history teachers to create learning journeys for their students on Singapore heritage with possible expansion to domestic and international tourists."},
-    { name: "Glue", goal: "A dynamic map that crawls social media and utilises archival content to connect contemporary experiences in Singapore with our shared past and present."}
+    { name: "Glue", goal: "A dynamic map that crawls social media and utilises archival content to connect contemporary experiences in Singapore with our shared past and present."},
   ];
-  res.render("pages/work" ,{
+
+  res.render("pages/work", {
     projects: projects,
   });
 });
